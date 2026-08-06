9f60e588701ddf5a23dbc64f5cecadf0759f4b82
Fifachaimong <fifasisa36@gmail.com>
2026-08-06 21:39:07 +0700

    Update path route admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import { CreateUserByAdmin, DeleteUser, EditUser } from '../controller/admin.js'
-import schema from '../schema/auth.js'
+import { registerSchema } from '../schema/auth.js'
 import { ValidateBody } from '../middleware/validate.js'
 
 const routess = express.Router()
 
-routess.post('/create', ValidateBody(schema), CreateUserByAdmin)
-routess.delete('/delete', DeleteUser)
-routess.put('/edit', EditUser)
+routess.post('/create', CreateUserByAdmin)
+routess.delete('/delete/:id', DeleteUser)
+routess.put('/edit/:id', EditUser)
 
 export default routess
