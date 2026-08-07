e41846516102c534e4d3b82036a4da2fd2e95003
Fifachaimong <fifasisa36@gmail.com>
2026-08-07 21:58:57 +0700

    Update fix poperty firtname in model  admin.js

--- models/admin.js
+++ models/admin.js
@@ -1,8 +1,8 @@
 import db from "../lib/db.js"
 
 export const CreateUserByAdminModel = async(data) => {
-    const { firtname, lastname, email, password, role } = data
-    const [result] = await db.query('INSERT INTO users(firtname, lastname, email, password, role) VALUES(?, ?, ?, ?, ?)', [firtname, lastname, email, password, role])
+    const { firstname, lastname, email, password, role } = data
+    const [result] = await db.query('INSERT INTO users(firstname, lastname, email, password, role) VALUES(?, ?, ?, ?, ?)', [firstname, lastname, email, password, role])
     return result
 }
 
@@ -12,10 +12,10 @@ export const DeleteUserByID = async (id) => {
 }
 
 export const EditUserByIDModel = async (data) => {
-    const { id, firtname, lastname, password, role } = data
+    const { id, firstname, lastname, password, role } = data
     const [result] = await db.query(
-        'UPDATE users SET firtname = COALESCE(?, firtname), lastname = COALESCE(?, lastname), password = COALESCE(?, password), role = COALESCE(?, role) WHERE id = ?',
-        [ firtname, lastname, password, role, id]
+        'UPDATE users SET firtname = COALESCE(?, firstname), lastname = COALESCE(?, lastname), password = COALESCE(?, password), role = COALESCE(?, role) WHERE id = ?',
+        [ firstname, lastname, password, role, id]
     )
     return result
 }
