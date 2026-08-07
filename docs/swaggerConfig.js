39b12070d5fb8bab7f1d8d169d020e5a8cb76dd9
Fifachaimong <fifasisa36@gmail.com>
2026-08-07 20:39:24 +0700

    Update swaggerConfig.js

--- docs/swaggerConfig.js
+++ docs/swaggerConfig.js
@@ -1,23 +1,33 @@
 import swaggerJSDoc from "swagger-jsdoc"
 
 const swaggerDefinition = {
-  openapi: '3.0.0',
+  openapi: "3.0.0",
   info: {
-    title: 'Express API Material Management',
-    version: '1.0.0',
+    title: "Express API Material Management",
+    version: "1.0.0",
   },
   servers: [
     {
-        url: 'http://localhost:3000',
-        description: 'server api '
-    }
-  ]
+      url: "http://localhost:3000",
+      description: "Server API",
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+      },
+    },
+  },
 }
 
 const options = {
   swaggerDefinition,
-  apis: ['./routes/*.js'],
+  apis: ["./routes/*.js"],
 }
 
 const swaggerSpec = swaggerJSDoc(options)
+
 export default swaggerSpec
