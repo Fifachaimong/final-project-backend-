e18d0624f3a05bd85bb7134288626140bbd2cc8f
Fifachaimong <fifasisa36@gmail.com>
2026-08-07 20:16:57 +0700

    Update route hr.js

--- routes/hr.js
+++ routes/hr.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import authMiddleware from '../middleware/authMiddleware.js'
+import { createPostSchema, editPostSchema, updateCandidateStatusSchema } from '../schema/hr.js'
+import { ValidateBody } from '../middleware/validate.js'
 import { CreatePost, DeletePost, EditPost, GetMember, GetMemberResumeResult, GetProfileByMember, UpdateCandidateStatus } from '../controller/hr.js'
 
 const route = express.Router()
@@ -7,9 +9,9 @@ const route = express.Router()
 route.get('/members', authMiddleware, GetMember)
 route.get('/member/profile', authMiddleware, GetProfileByMember)
 route.get('/members/:id', authMiddleware, GetMemberResumeResult)
-route.post('/posts', authMiddleware , CreatePost)
-route.put('/posts', authMiddleware, EditPost)
-route.put('/members/:id', authMiddleware, UpdateCandidateStatus)
+route.post('/posts', authMiddleware, ValidateBody(createPostSchema), CreatePost)
+route.put('/posts/:id', authMiddleware, ValidateBody(editPostSchema), EditPost)
+route.put('/members/:id', authMiddleware, ValidateBody(updateCandidateStatusSchema), UpdateCandidateStatus)
 route.delete('/posts', authMiddleware, DeletePost)
 
 export default route
