52064065ae17042800ad7e82f2c26266154d54b5
Fifachaimong <fifasisa36@gmail.com>
2026-08-08 23:19:10 +0700

    fix: update sql file to match database changes

--- database/schema.sql
+++ database/schema.sql
@@ -9,7 +9,7 @@ USE resume_ai_platform;
 -- =========================
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
+    firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     phone VARCHAR(15) DEFAULT NULL,
