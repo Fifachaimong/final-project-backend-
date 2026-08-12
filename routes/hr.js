27f4a6e087ffd4416af5f94020e83e867228653b
Fifachaimong <fifasisa36@gmail.com>
2026-08-13 00:13:14 +0700

    Add swagger doccument hr feat: get members

--- routes/hr.js
+++ routes/hr.js
@@ -15,3 +15,48 @@ route.put('/members/:id', authMiddleware, ValidateBody(updateCandidateStatusSche
 route.delete('/posts', authMiddleware, DeletePost)
 
 export default route
+
+/**
+ * @swagger
+ * /hr/members:
+ *   get:
+ *     summary: Get HR members
+ *     description: Retrieve a list of members for HR.
+ *     tags:
+ *       - hr
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Members retrieved successfully
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: "Get my member succeed"
+ *               data:
+ *                 - Post_id: 6
+ *                   user_firstname: "Golf"
+ *                   user_lastname: "Matin"
+ *                   ai_score: "65.90"
+ *
+ *       401:
+ *         description: Unauthorized - Missing or invalid token
+ *         content:
+ *           application/json:
+ *             examples:
+ *               missing_token:
+ *                 summary: Missing token
+ *                 value:
+ *                   message: "Unauthorization"
+ *               invalid_token:
+ *                 summary: Invalid token
+ *                 value:
+ *                   message: "Invalid token"
+ *
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: "Internal server error"
+ */
