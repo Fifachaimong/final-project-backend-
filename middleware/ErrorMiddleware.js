72ddd6c29b23afc0a25de7099c69ab03f5ff5174
Fifachaimong <fifasisa36@gmail.com>
2026-08-22 20:28:59 +0700

    fix: handle validation errors by field

--- middleware/ErrorMiddleware.js
+++ middleware/ErrorMiddleware.js
@@ -2,7 +2,8 @@ const ErrorMiddleware = (err, req, res, next) => {
     console.error(err)
     const statusError = err.status || 500
     res.status(statusError).json({
-        message : err.message || 'Internal Server Error'
+        message : err.message || 'Internal Server Error',
+        field : err.field || null
     })
 }
 
--- middleware/validate.js
+++ middleware/validate.js
@@ -5,7 +5,7 @@ export const ValidateBody = (schema) => {
         const data = req.body
         for (const key in data) {
             if (!(key in schema)) {
-                return next(new AppError('You are not allowed to enter this information.', 400))
+                return next(new AppError('You are not allowed to enter this information.', 400, key))
             }
         }
         
@@ -13,7 +13,7 @@ export const ValidateBody = (schema) => {
             const rule = schema[key]
             const value = data[key]
             if (rule.required && value === undefined) {
-                return next(new AppError(`Please enter your ${key}`, 400))
+                return next(new AppError(`Please enter your ${key}`, 400, key))
             }
 
             if (!rule.required && value === undefined) {
@@ -26,26 +26,26 @@ export const ValidateBody = (schema) => {
 
             if (rule.type === 'string') {
                 if (typeof value !== 'string' || !value.trim()) {
-                    return next(new AppError(`${key} must be a string`, 400))
+                    return next(new AppError(`${key} must be a string`, 400, key))
                 }
             }
             
             if (rule.type === 'number') {
                 if (typeof value !== 'number') {
-                    return next(new AppError(`${key} must be a number`, 400))
+                    return next(new AppError(`${key} must be a number`, 400, key))
                 }
             }
 
             if (Array.isArray(rule.type)) {
                 if (!rule.type.includes(value)) {
-                    return next(new AppError(`${key} must be a ${rule.type}`, 400))
+                    return next(new AppError(`${key} must be a ${rule.type}`, 400, key))
                 }
             }
 
             if (rule.validate) {
                 for (const item of rule.validate) {
                     if (!item.check(value)) {
-                        return next(new AppError(item.message, 400))
+                        return next(new AppError(item.message, 400, key))
                     }
                 }
             }
